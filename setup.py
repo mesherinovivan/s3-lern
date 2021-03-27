289f655f4a0f09ea8525209bce3c37e9781b25aa
Ivan Mescherinov <mesherinovivan@mail.ru>
2021-03-28 01:00:04 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='s3-pack',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    author='Ivan Mescherinov',
+    author_email='',
+    license='unlicense',
+    packages=['ptolemaios'],
+    zip_safe=False
+)
