d5ebb7a2fbdb43661fded151ef5b04436c1c7cfd
Ivan Mescherinov <mesherinovivan@mail.ru>
2021-03-28 01:10:06 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='s3-pack',
+    name='s3pack',
     version='0.0.1',
     description='My private package from private github repo',
     url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
